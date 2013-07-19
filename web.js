b80f558b93821b3c0d89c695715b702b1035e635
Yuanda He <yuandahe1@gmail.com>
2013-07-19 14:06:36 +0000

    Try to read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,17 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+index = fs.readFile('/home/ubuntu/bitstarter/index.html', function (err, data){
+    if(err) throw err;
+    console.log(data);
+});
+
+buffer = new Buffer(index);
+ 
+var buffer = new Buffer(
+
 app.get('/', function(request, response) {
-  response.send('Hello World, Tony!');
+  response.send(buffer.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
